068bd2637a9aad3e722852fa26c20eac2d2b0179
Rahul Prabala <rprabala@stanford.edu>
2016-02-24 16:05:23 -0800

    made at .28ns, going to start adding more

--- assignment/assignment2/gold/rastBBox_fix_sv.c
+++ assignment/assignment2/gold/rastBBox_fix_sv.c
@@ -96,7 +96,10 @@ int rastBBox_bbox_check( int   v0_x,     //uPoly
   ll_y = ( !b_y[1] && !b_y[3] )                 ? poly.v[2].x[1] :  ll_y ;
   ll_y = ( !b_y[2] && !b_y[4] && !b_y[5] && q ) ? poly.v[3].x[1] :  ll_y ;
 
-  valid = (! ((ll_x > screen_w) || (ll_y > screen_h) || (ur_x < 0) || (ur_y < 0)) ) && valid_Poly; 
+  //add in backface culling
+  //int forwardfacing = ((poly.v[1].x[0] - poly.v[0].x[0])*(poly.v[2].x[1] - poly.v[1].x[1]) - (poly.v[2].x[0] - poly.v[1].x[0])*(poly.v[1].x[1] - poly.v[0].x[1])) <= 0;
+
+  valid = (! ((ll_x > screen_w) || (ll_y > screen_h) || (ur_x < 0) || (ur_y < 0)) ) && valid_Poly;// && forwardfacing; 
 
 
   //Clamp BBox
